577f2aded50f7bd6f0d4891156674e3bc18d9b1d
jussiheikkinen <jussih91@gmail.com>
2020-04-13 15:22:21 +0300

    fix

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -5,5 +5,8 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
+
+DOCKER_PATH=$DOCKER_USER/$DOCKERHUB_REPO:$TAG
+
+docker build -f Dockerfile -t $DOCKER_PATH .
 docker push $TRAVIS_REPO_SLUG:$TAG
